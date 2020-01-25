53fe0f8694ca752aa00bfedd6124027534b13c6e
MayThuZaw <maythuzaw152003@gmail.com>
2020-01-25 12:31:01 +0630

    variable

---
+++ Variable.py
@@ -0,0 +1,93 @@
+#Variable
+print("Hellow World")
+
+
+>>> 2 + 2
+4
+>>> 50 - 5 * 6
+20
+>>> (50 - 5 * 6) / 4
+5.0
+>>> round(5.0)
+5
+>>> round(5.023434,3)
+5.023
+>>>
+
+
+>>> 17 / 3
+5.66666666667
+>>> 17 // 3
+5
+>>> 18 // 4
+4
+>>> 20 // 5
+4
+>>> 17 % 3
+2
+>>> 17 * 4 % 3
+2
+
+int - integer - 1, 2, 3, ---> 308930 etc
+float - 	 - 1.0, 5.9, 6.0, 63.23,   etc
+string - 	 - 'Hello', 'World' , 'A','B',
+
+a=1
+a (variable) = (assign) 1 (value)
+
+width = 20
+height = 5 * 9
+volume = width * height 
+volume
+
+
+
+
+
+
+
+
+print('spam eggs')
+print(' don\'t')
+print("does't")
+print('"Yes"')
+print("\"Yes,\" they said.")
+print('"Isn\'t," they said')
+
+print('"Isn\'t, they said"')
+"'Hellow World'"
+
+
+# \n =newLine
+
+s = 'First Line. \nSecond LIne.'
+
+
+
+
+
+
+* / % + - (operator)
+
+addition = 75 + 25 
+subtraction = 204 - 204
+multiplication = 100 * 0.5
+division = 90 / 9 
+power of 2 = 3 ** 2
+the remaindr of the division = 10 % 3
+
+Variable
+
+* or % 
+5.*.3.+.2.%.3.
+
+5 * 3 + 3 % 4
+
+6 * 9 - 4 * 5 % 2
+
+7 * 5 - 6 * 3 % 3
+
+6.*.5 - 7 * 4 / 4 % 2
+
+8 * 9 / 4 * 5 % 3 - 2
+
